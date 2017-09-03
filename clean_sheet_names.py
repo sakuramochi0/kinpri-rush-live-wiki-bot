44d92ece70e3fda3c644cc6c73803fe8a568bef0
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-09-04 05:51:26 +0900

    スプレッドシートのシートと id 一覧を処理するユーティリティを追加

---
+++ clean_sheet_names.py
@@ -0,0 +1,16 @@
+import re
+
+with open('sheet_names.txt') as f:
+    rows = f.read().split('\n')
+
+remove_time = lambda row: re.sub(r'\[.+?\] ', '', row)
+
+for id, name, id_ko, name_ko in zip(
+        map(remove_time, rows[::2]),
+        map(remove_time, rows[1::4]),
+        map(remove_time, rows[2::4]),
+        map(remove_time, rows[3::4]),
+):
+    print('=hyperlink("#gid={}", "{}")\t=hyperlink("#gid={}", "{}")'.format(
+        id, name, id_ko, name_ko,
+    ))
